a5946bc7bf6c4413d6243e143c3f26666a09f5fd
Abdell Gautier <abdellgautier@hotmail.com>
2021-10-04 04:27:18 +0000

    Created the deck of cards, plus shuffling done.

--- p1/README.md
+++ p1/README.md
@@ -3,11 +3,13 @@
 + URL: <http://e2p1.abdellgautier.com>
 
 ## Game planning
++ Create an array containing all cards
 + Each player starts with half the deck (26 cards), shuffled in a random order.
 + For each round, a card is picked from the “top” of each player’s cards.
 + Whoevers card is highest wins that round and keeps both cards.
 + If the two cards are of the same value, it’s a tie and those cards are discarded.
 + The player who ends up with 0 cards is the loser.
++ Report the results in the view
 
 ## Outside resources
 
--- p1/index.php
+++ p1/index.php
@@ -4,4 +4,81 @@
 $roundsPlayed = 106;
 $winner = "Player 2";
 
+// Define the card suits
+$cardSuits = [
+    "Clubs",
+    "Diamonds",
+    "Hearts",
+    "Spades"
+];
+
+// Define the card ranks
+$cardRanks = [
+    "Ace" => 1,
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "10" => 10,
+    "Jack" => 11,
+    "Queen" => 12,
+    "King" => 13
+];
+
+// Will hold all the 52 cards
+$deck = [];
+
+// Build our stack of cards, based on the suits and ranks
+foreach ($cardSuits as $cSuit) {
+    foreach ($cardRanks as $cRank => $value) {
+        $deck[$cSuit . "_" . $cRank] = $value;
+    }
+}
+
+// Shuffle our deck, keeping the associative array intact.
+// If we shuffle using the regular 'shuffle()' function, we lose the custom keys
+// Credit to ahmad at: https://www.php.net/manual/en/function.shuffle.php#94697
+shuffle_assoc($deck);
+
+$playerCards = [];
+$computerCards = [];
+
+// Set the initial target to player when dealing cards
+$dealTo = "Player";
+
+// Assign the cards, alternating between player and computer
+foreach ($deck as $card => $value) {
+    if ($dealTo == "Player") {
+        $playerCards[$card] = $value;
+        $dealTo = "Computer";
+    }
+    else {
+        $computerCards[$card] = $value;
+        $dealTo = "Player";
+    }
+}
+
+// Player and Computer draw a card
+$playerDraw = array_pop($playerCards);
+$computerDraw = array_pop($computerCards);
+
+// Credit to ahmad at: https://www.php.net/manual/en/function.shuffle.php#94697
+function shuffle_assoc(&$array) {
+    $keys = array_keys($array);
+
+    shuffle($keys);
+
+    foreach($keys as $key) {
+        $new[$key] = $array[$key];
+    }
+
+    $array = $new;
+
+    return true;
+}
+
 require 'index-view.php';
